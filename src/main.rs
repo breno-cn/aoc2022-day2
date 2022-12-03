e21142a1428a2248f1ee59f1bc2e7eb263a5d7cf
Breno Caldeira Nascimento <breno.cn@hotmail.com>
2022-12-03 19:33:11 -0300

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::{panic, str};
 use std::{collections::HashMap};
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
 enum Hand { Rock, Paper, Scissor }
 
 fn str_to_hand(hand: &str) -> Hand {
@@ -26,6 +26,41 @@ fn parse_input(filepath: &str) -> Vec<(Hand, Hand)> {
             (str_to_hand(split[0]), str_to_hand(split[1]))
         })
         .collect::<Vec<(Hand, Hand)>>()
+}
+
+fn parse_input_part2(filepath: &str) -> Vec<(Hand, Hand)> {
+    std::fs::read_to_string(filepath)
+        .unwrap()
+        .split("\n")
+        .map(|line| {
+            let split = line.split(" ").collect::<Vec<&str>>();
+
+            let oponent = str_to_hand(split[0]);
+            let me = determine_hand(&oponent, split[1]);
+
+            (oponent, me)
+        })
+        .collect::<Vec<(Hand, Hand)>>()
+}
+
+fn determine_hand(oponent: &Hand, me: &str) -> Hand {
+    match me {
+        "X" => match oponent {
+            Hand::Rock => Hand::Scissor,
+            Hand::Paper => Hand::Rock,
+            Hand::Scissor => Hand::Paper
+        },
+
+        "Y" => oponent.clone(),
+
+        "Z" => match oponent {
+            Hand::Rock => Hand::Paper,
+            Hand::Paper => Hand::Scissor,
+            Hand::Scissor => Hand::Rock
+        },
+
+        _ => panic!("really?...")
+    }
 }
 
 fn play_match(oponent: &Hand, me: &Hand) -> i32 {
@@ -68,6 +103,8 @@ fn solve(games: Vec<(Hand, Hand)>) -> i32 {
 
 fn main() {
     let games = parse_input("input.txt");
-
     println!("{:}", solve(games));
+
+    let part2_games = parse_input_part2("input.txt");
+    println!("{:}", solve(part2_games));
 }
